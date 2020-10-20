06369c2ea29877378e022e33c81d654006df0adf
havrama4 <71828972+havrama4@users.noreply.github.com>
2020-10-20 15:22:58 +0200

    Add files via upload

---
+++ trideni.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <stdio.h>
+
+
+typedef std::vector<std::string> seznam_slov;
+
+void nacti (std::string nazev, seznam_slov & jmena);
+
+void quick_sort (std::string seznam_slov *jmena);
+
+int rozhodnuti (bool true, bool false);
+
+int printf (char *jmena [argc, argv]);
+
+
